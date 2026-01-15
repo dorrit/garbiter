8b659bdc5840a730f4a074a91319305faa8d99c9
dorrit <radithamzah12@gmail.com>
2026-01-16 04:56:44 +0700

    system: new SetIdentity and PrintResource

--- system.go
+++ system.go
@@ -17,3 +17,25 @@ func (s *SystemAPI) PrintIdentity() (*model.Identity, error) {
 		Name: res["name"],
 	}, nil
 }
+
+// SetIdentity sets the device identity.
+func (s *SystemAPI) SetIdentity(name string) error {
+	_, err := s.svc.Run("/system/identity/set", "=name="+name)
+	return err
+}
+
+// PrintResource returns the device resource information in a typed struct.
+func (s *SystemAPI) PrintResource() (*model.Resource, error) {
+	res, err := s.svc.Run("/system/resource/print")
+	if err != nil {
+		return nil, err
+	}
+
+	return &model.Resource{
+		CPU:         res["cpu"],
+		CPULoad:     res["cpu-load"],
+		FreeMemory:  res["free-memory"],
+		TotalMemory: res["total-memory"],
+		Uptime:      res["uptime"],
+	}, nil
+}
