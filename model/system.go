fb404ce07445fe39efc86cdd2fdd40a4032bb88c
dorrit <radithamzah12@gmail.com>
2026-01-16 04:57:15 +0700

    model: system: new Resource

--- model/system.go
+++ model/system.go
@@ -3,3 +3,11 @@ package model
 type Identity struct {
 	Name string
 }
+
+type Resource struct {
+	CPU         string
+	CPULoad     string
+	FreeMemory  string
+	TotalMemory string
+	Uptime      string
+}
